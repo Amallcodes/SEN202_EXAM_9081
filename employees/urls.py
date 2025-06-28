c5ed378933193c97389a5b618899ea07d6eec3bc
Veritas 9081 <9081@veritas.edu.ng>
2025-06-28 10:59:48 +0100

    create url patterns

--- employees/urls.py
+++ employees/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
-from .views import StaffRoleList
+from .views import (
+    StaffRoleList,
+    ManagerListCreateView, ManagerRetrieveUpdateDestroyView,
+    InternListCreateView, InternRetrieveUpdateDestroyView,
+)
 
 urlpatterns = [
     path('staff/', StaffRoleList.as_view(), name='staff-role-list'),
+
+    # CRUD for Manager
+    path('managers/', ManagerListCreateView.as_view(), name='manager-list-create'),
+    path('managers/<int:pk>/', ManagerRetrieveUpdateDestroyView.as_view(), name='manager-detail'),
+
+    # CRUD for Intern
+    path('interns/', InternListCreateView.as_view(), name='intern-list-create'),
+    path('interns/<int:pk>/', InternRetrieveUpdateDestroyView.as_view(), name='intern-detail'),
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 from .models import Manager, Intern
 from .serializers import ManagerSerializer, InternSerializer
 
@@ -21,3 +22,19 @@ class StaffRoleList(APIView):
             data.append(serializer.data)
 
         return Response(data)
+
+class ManagerListCreateView(generics.ListCreateAPIView):
+    queryset = Manager.objects.all()
+    serializer_class = ManagerSerializer
+
+class ManagerRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Manager.objects.all()
+    serializer_class = ManagerSerializer
+
+class InternListCreateView(generics.ListCreateAPIView):
+    queryset = Intern.objects.all()
+    serializer_class = InternSerializer
+
+class InternRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Intern.objects.all()
+    serializer_class = InternSerializer
